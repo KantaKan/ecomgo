45c04b179c007f7c98064239caec189633b12764
KantaKan <kantapon3682@gmail.com>
2026-06-23 02:43:40 +0700

    error checking and then somelog when fail to start sever

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,24 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 
 func main() {
 	cfg := config {
 		addr: ":8080",
-		db: dbConfig{}
+		db: dbConfig{},
 
 	}
 	api := application{
 		config: cfg,
 	}
 
-	api.run(api.mount())
+	if err := api.run(api.mount()); err != nil {
+		log.Fatal("server has failed to start:", err)
+		os.Exit(1)
+		
+		}
+}
