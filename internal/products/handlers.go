473aa30ea1c1ce05107b8e798fafe62471d22136
KantaKan <fiakantapon@gmail.com>
2026-06-27 03:31:10 +0700

    done writer function and some product listing

--- internal/products/handlers.go
+++ internal/products/handlers.go
@@ -1,6 +1,10 @@
 package products
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/KantaKan/ecomgo/internal/json"
+)
 
 type handler struct {
 	service Service
@@ -15,4 +19,10 @@ func NewHandler(service Service) *handler {
 func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
 	//call the service  > list products
 	//2 return JSON in http response
+	products :=  struct {
+		Products []string `json:"products"`
+	}{}
+
+	json.Write(w, http.StatusOK,products)
+	
 }
