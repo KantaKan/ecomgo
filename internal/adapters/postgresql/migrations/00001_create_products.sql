8cf177319e65f474c97a6eeda6d7ef0f71b60c6d
KantaKan <fiakantapon@gmail.com>
2026-06-27 18:40:26 +0700

    added migration and some model queriy and db

--- internal/adapters/postgresql/migrations/00001_create_products.sql
+++ internal/adapters/postgresql/migrations/00001_create_products.sql
@@ -1,5 +1,11 @@
 -- +goose Up
-SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS products (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL ,
+    price_in_centers INTEGER NOT NULL CHECK ( price_in_centers >= 0 ),
+    quantity INTEGER NOT NULL DEFAULT 0,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT  now()
+);
 
 -- +goose Down
 SELECT 'down SQL query';
